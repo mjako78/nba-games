af407c733de895c0060bd0371330268b62872614
Mauro Giacomini <mauro.giacomini@axiante.com>
2022-03-08 08:47:13 +0100

    Add dump method to cli test

--- utils/api.js
+++ utils/api.js
@@ -12,64 +12,16 @@ function getGames(date) {
     let message = Soup.Message.new('GET', url);
     message.request_headers.set_content_type("application/json", null);
     let responseCode = _session.send_message(message);
-    let games;
+    let games = []
     if (responseCode == 200) {
         try {
             games = JSON.parse(message.response_body.data).data;
-            // log(message['response-body'].data);
-            // log("---");
-            // log(JSON.parse(message['response-body'].data).data);
-            // let out = JSON.parse(message['response-body'].data);
-            // log(out);
         } catch (error) {
             log("Error parsing response");
             log(error);
         }
     } else {
-        log("Error getting response");
+        log("Error getting response; responseCode: " + responseCode);
     }
     return games;
-    // if (games) {
-    //     log("Games played: " + games.length);
-    //     games.forEach(game => {
-    //         log("");
-    //         log("*** GAME " + game.id + " ***");
-    //         log(game.home_team.full_name.padEnd(30) + game.home_team_score.toString().padStart(3));
-    //         log(game.visitor_team.full_name.padEnd(30) + game.visitor_team_score.toString().padStart(3));
-    //         log("*******************");
-    //     });
-    // }
-
-//   return games;
-//   _session.queue_message(request, function(_session, message) {
-//       let response = JSON.parse(message.response_body.data);
-//       let games = response.data;
-//       log("Games played: " + games.length);
-//       games.forEach(game => {
-//           log("\n*** GAME " + game.id + " ***");
-//           log(game.home_team.full_name.padEnd(30) + game.home_team_score.toString().padStart(3));
-//           log(game.visitor_team.full_name.padEnd(30) + game.visitor_team_score.toString().padStart(3));
-//           log("*******************");
-//       });
-//   });
-}
-
-// FIXME: Check this out
-function send_request(url, type='GET') {
-  let message = Soup.Message.new(type, url);
-  message.request_headers.append(
-      'Authorization',
-      `Bearer ${token}`
-  )
-  message.request_headers.set_content_type("application/json", null);
-  let responseCode = soupSyncSession.send_message(message);
-  let out;
-  if(responseCode == 200) {
-      try {
-          out = JSON.parse(message['response-body'].data);
-      } catch(error) {
-          log(error);
-      }
-  }
-  return out;
 }
